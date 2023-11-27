1f9286256ddb8c06ca79f6084e4133dd498bea70
YuraSopa <149674502+YuraSopa@users.noreply.github.com>
2023-11-27 19:59:08 +0200

    Update Repository.kt

--- app/src/main/java/com/example/myslotapp/domain/Repository.kt
+++ app/src/main/java/com/example/myslotapp/domain/Repository.kt
@@ -2,8 +2,6 @@ package com.example.myslotapp.domain
 
 interface Repository {
 
-    fun setBet(bet: Bet): Int
-
     fun createGame(bet: Int, credit: Int): Game
 
     fun spinReels(game: Game): Game
