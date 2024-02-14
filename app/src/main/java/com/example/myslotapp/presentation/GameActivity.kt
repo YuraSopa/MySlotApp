cce65126f19c4d74e27a69b850443cd3dbe72623
YuraSopa <aspiresopa@gmail.com>
2024-02-14 09:01:21 +0200

    Added text animation

--- app/src/main/java/com/example/myslotapp/presentation/GameActivity.kt
+++ app/src/main/java/com/example/myslotapp/presentation/GameActivity.kt
@@ -1,11 +1,15 @@
 package com.example.myslotapp.presentation
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.example.myslotapp.R
 import com.example.myslotapp.databinding.ActivityGameBinding
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -15,26 +19,46 @@ class GameActivity : AppCompatActivity() {
 
     private val viewModel = GameViewModel()
 
+    var previousBet: Int? = null
+    var previousCredit: Int? = null
+    var previousWin: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         lifecycleScope.launch {
             viewModel.game.collectLatest {
-                binding.tvBet.text = String.format("$%s", it.bet)
-                binding.tvCredit.text = String.format("$%s", it.credit)
-                binding.tvWin.text = String.format("$%s", it.win)
+                if (it.bet != previousBet) {
+                    binding.tvBet.startAnimation(createAnimation())
+                    binding.tvBet.text = String.format("$%s", it.bet)
+                    previousBet = it.bet
+                }
+
+                if (it.credit != previousCredit) {
+                    binding.tvCredit.startAnimation(createAnimation())
+                    binding.tvCredit.text = String.format("$%s", it.credit)
+                    previousCredit = it.credit
+                }
+
+                if (it.win != previousWin) {
+                    binding.tvWin.startAnimation(createAnimation())
+                    binding.tvWin.text = String.format("$%s", it.win)
+                    previousWin= it.win
+                }
             }
         }
 
-        lifecycleScope.launch{
-            viewModel.isSpinning.collect{
-                if (it){
+        lifecycleScope.launch {
+            viewModel.isSpinning.collect {
+                if (it) {
                     binding.ivBetOne.setImageResource(R.drawable.btn_one_disabled)
                     binding.ivBetMax.setImageResource(R.drawable.btn_max_disabled)
                     binding.ivSpin.setImageResource(R.drawable.btn_spin_disabled)
-                }else{
+                } else {
                     binding.ivBetOne.setImageResource(R.drawable.btn_one_default)
                     binding.ivBetMax.setImageResource(R.drawable.btn_max_default)
                     binding.ivSpin.setImageResource(R.drawable.btn_spin_default)
@@ -52,23 +76,42 @@ class GameActivity : AppCompatActivity() {
         }
 
         binding.ivBetOne.setOnClickListener {
+            clickAnimation(
+                binding.ivBetOne, R.drawable.btn_one_pressed, R.drawable.btn_one_default
+            )
             viewModel.changeBet()
         }
 
         binding.ivBetMax.setOnClickListener {
+            clickAnimation(
+                binding.ivBetMax, R.drawable.btn_max_pressed, R.drawable.btn_max_default
+            )
             viewModel.setMaxBet()
         }
 
+        binding.ivPayTable.setOnClickListener {
+            clickAnimation(
+                binding.ivPayTable, R.drawable.btn_table_pressed, R.drawable.btn_table_default
+            )
+            launchFragment(TableFragment())
+        }
 
         binding.ivSpin.setOnClickListener {
             viewModel.startSpin()
         }
+    }
 
-        binding.ivPayTable.setOnClickListener {
-            launchFragment(TableFragment())
+    private fun clickAnimation(imageView: ImageView, resIdPressed: Int, resIdDefault: Int) {
+        lifecycleScope.launch {
+            imageView.setImageResource(resIdPressed)
+            delay(200)
+            imageView.setImageResource(resIdDefault)
         }
     }
 
+    private fun createAnimation(): Animation {
+        return AnimationUtils.loadAnimation(this, R.anim.text_animation)
+    }
 
     private fun launchFragment(fragment: Fragment) {
         supportFragmentManager
